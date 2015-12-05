9a32728cba867cf0ebb3764e92dcbd929e2d55de
Bommer <quangthi_90@yahoo.com.vn>
2015-12-06 00:38:11 +0700

    - Create project
    Signed-off-by: Bommer <quangthi_90@yahoo.com.vn>

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/sosanhgia/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/sosanhgia/');
+
+// DIR
+define('DIR_APPLICATION', 'F:/Program/xampp/htdocs/SoSanhGia/catalog/');
+define('DIR_SYSTEM', 'F:/Program/xampp/htdocs/SoSanhGia/system/');
+define('DIR_LANGUAGE', 'F:/Program/xampp/htdocs/SoSanhGia/catalog/language/');
+define('DIR_TEMPLATE', 'F:/Program/xampp/htdocs/SoSanhGia/catalog/view/theme/');
+define('DIR_CONFIG', 'F:/Program/xampp/htdocs/SoSanhGia/system/config/');
+define('DIR_IMAGE', 'F:/Program/xampp/htdocs/SoSanhGia/image/');
+define('DIR_CACHE', 'F:/Program/xampp/htdocs/SoSanhGia/system/storage/cache/');
+define('DIR_DOWNLOAD', 'F:/Program/xampp/htdocs/SoSanhGia/system/storage/download/');
+define('DIR_LOGS', 'F:/Program/xampp/htdocs/SoSanhGia/system/storage/logs/');
+define('DIR_MODIFICATION', 'F:/Program/xampp/htdocs/SoSanhGia/system/storage/modification/');
+define('DIR_UPLOAD', 'F:/Program/xampp/htdocs/SoSanhGia/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'sosanhgia');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '');
